9dc196281be880bf77e8aa93c2afdd095763c274
Alex Gibson <oakmonttech@gmail.com>
2017-03-08 17:51:03 -0500

    Example for SO Answer

---
+++ StackViewHorizontal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewHorizontal
+//
+//  Created by Alex Gibson on 3/8/17.
+//  Copyright © 2017 AG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
